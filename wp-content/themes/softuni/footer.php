7d6da0dd0a2aaf1831d8ac58a538b79ddcdc7f25
DaniBacheva <y.bacheva@abv.bg>
2024-11-14 21:54:41 +0200

    Add initial header functions

--- wp-content/themes/softuni/footer.php
+++ wp-content/themes/softuni/footer.php
@@ -137,6 +137,7 @@
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/plugins.js"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
 
+    <?php wp_footer(); ?>
 </body>
 
 </html>
--- wp-content/themes/softuni/header.php
+++ wp-content/themes/softuni/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html class="no-js" lang="en">
+<html class="no-js" <?php language_attributes(); ?>>
 
 <head>
-	<title>Archi - Free Architecture Portfolio HTML Template</title>
+	<title><?php wp_title(); ?></title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,6 +23,7 @@
 	<link
 		href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap"
 		rel="stylesheet">
+    <?php wp_head(); ?>
 </head>
 
 <body>
