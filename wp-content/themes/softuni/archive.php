765d81ad542b88cf36dd14cb8c5db343711b8ca7
DaniBacheva <y.bacheva@abv.bg>
2024-11-15 19:25:15 +0200

    trim archive

--- wp-content/themes/softuni/archive.php
+++ wp-content/themes/softuni/archive.php
@@ -4,7 +4,6 @@
 	<div class="container">
 
 		<div class="row">
-
 			<div class="col">
 				<div class="section-header">
 					<h2 class="section-title"><?php echo get_the_archive_title(); ?></h2>
@@ -22,6 +21,7 @@
 					<?php while( have_posts() ) : the_post(); ?>
 						<div class="col-md-4">
 							<article class="post-item">
+
 								<?php if( has_post_thumbnail() ) : ?>
 									<figure>
 										<a href="<?php echo get_the_permalink(); ?>" class="image-hvr-effect">
@@ -29,35 +29,31 @@
 										</a>
 									</figure>
 								<?php endif; ?>
+
 								<div class="post-content">
 									<div class="meta-date"><?php echo get_the_date(); ?></div>
 									<h3 class="post-title"><a href="<?php echo get_the_permalink(); ?>"><?php the_title(); ?></a></h3>
 									<p><?php echo get_the_excerpt(); ?></p>
 								</div>
 							</article>
-
 						</div>
 					<?php endwhile; ?>
+
 					</div>
 				</div>
 			<?php else : ?>
 				no have post
 			<?php endif; ?>	
-			</div>
-
 		</div>
+	</div>
 
-		<div class="row">
-			<div class="col">
-				<div class="btn-wrap align-center">
-					<a href="#" class="btn btn-xlarge btn-accent btn-rounded">View all blog</a>
-				</div>
+	<div class="row">
+		<div class="col">
+			<div class="btn-wrap align-center">
+				<a href="#" class="btn btn-xlarge btn-accent btn-rounded">View all blog</a>
 			</div>
 		</div>
-
 	</div>
 </section>
-
-
 
 <?php get_footer(); ?>	
