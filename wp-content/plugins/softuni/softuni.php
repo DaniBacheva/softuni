1c201b75f7889c6bede33814125eefabfd335d1a
DaniBacheva <y.bacheva@abv.bg>
2024-11-18 19:26:28 +0200

    add cpt testimonial

--- wp-content/plugins/softuni/softuni.php
+++ wp-content/plugins/softuni/softuni.php
@@ -9,5 +9,12 @@
 
  //echo 'Hello from my plugin';
 
+ if ( ! defined( 'SOFTUNI_PLUGIN_VERSION' ) ) {
+    define( 'SOFTUNI_PLUGIN_VERSION', '0.1' );
+}
+if ( ! defined( 'SOFTUNI_PLUGIN_ASSETS_URL' ) ) {
+    define( 'SOFTUNI_PLUGIN_ASSETS_URL',  plugin_dir_url( __FILE__ ) . 'assets' );
+}
 
  require 'includes/cpt-portfolio.php';
+ require 'includes/cpt-testimonial.php';
