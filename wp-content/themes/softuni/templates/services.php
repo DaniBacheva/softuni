68ee9b8fe1e23572df60cdecdb47039e31bbf851
DaniBacheva <y.bacheva@abv.bg>
2024-11-20 22:31:06 +0200

    Create Service template

---
+++ wp-content/themes/softuni/templates/services.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Template Name: Services
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php
+	get_template_part( 'partials/about', 'section' );
+?>
+
+<?php
+	get_template_part( 'partials/services', 'section' );
+?>	
+
+<?php
+	get_template_part( 'partials/contact' );
+?>
+
+<?php
+	get_template_part( 'partials/testimonial' );
+?>
+
+<?php get_footer(); ?>	
