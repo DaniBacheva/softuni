1d09014562f327bd123704e5ed051dc369ba3e02
DaniBacheva <y.bacheva@abv.bg>
2024-11-22 19:50:39 +0200

    dynamic show of image

--- wp-content/plugins/softuni/includes/functions.php
+++ wp-content/plugins/softuni/includes/functions.php
@@ -10,10 +10,33 @@ function display_portfolio_title( $atts ) {
    // return get_the_title( 74 ); 
  
     $atts = shortcode_atts( array(
-        'id'   => 'id',
-
+        'id'         => 'id',
+        'show_image' => 'show_image'
     ), $atts, 'bartag' );
-    return get_the_title( $atts['id'] ); 
+
+    if ( ! empty( $atts['id'] ) ){
+        $title = get_The_title( $atts['id'] );
+    }
+
+    if ( ! empty( $atts['show_image'] ) ) {
+        $image = get_the_post_thumbnail_url( $atts['id'] );
+    }
+
+    $content = '<div class="shortcode-class">';
+
+    if ( ! empty( $title) ) {
+        $content .=$title;
+    }
+
+    if ( ! empty( $image ) ) {
+        $content .= '<img src="'. $image .'">';
+    }
+
+    $content .= '</div>';
+
+    //var_dump( $image)
+
+    return $content; 
 
 }
 
