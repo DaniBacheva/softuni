50982e655ac457e39a834ffbd237ab5998719fb6
DaniBacheva <y.bacheva@abv.bg>
2024-11-22 19:26:32 +0200

    add attributes to show title

--- wp-content/plugins/softuni/includes/functions.php
+++ wp-content/plugins/softuni/includes/functions.php
@@ -6,8 +6,15 @@
 * @return void
 */
 
-function display_portfolio_title( $id=0 ) {
-    return get_the_title( 74 ); 
+function display_portfolio_title( $atts ) {
+   // return get_the_title( 74 ); 
+ 
+    $atts = shortcode_atts( array(
+        'id'   => 'id',
+
+    ), $atts, 'bartag' );
+    return get_the_title( $atts['id'] ); 
+
 }
 
 add_shortcode( 'display_portfolio_title', 'display_portfolio_title' );
