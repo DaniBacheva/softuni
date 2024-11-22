087380d3e0ae29a7a8c29abc53d0c77b6430142c
DaniBacheva <y.bacheva@abv.bg>
2024-11-22 18:45:31 +0200

    dynamic load style.css

--- wp-content/themes/softuni/functions.php
+++ wp-content/themes/softuni/functions.php
@@ -6,9 +6,14 @@ add_theme_support ( 'title-tag' );
 
 add_post_type_support( 'excerpt', array() );
 
+
+
+add_action( 'wp_enqueue_scripts', 'softuni_enqueue_assets' );
+
 function softuni_enqueue_assets() {
-  //  
+    wp_enqueue_style( 'softuni-main-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0.0' );
 }
+
 
 
 function softuni_display_latest_posts($number_of_posts=3 ){
