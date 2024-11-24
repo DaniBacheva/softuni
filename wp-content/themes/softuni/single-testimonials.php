6f55b938ab68e0f3067c84d2cf8107cbd7132b68
DaniBacheva <y.bacheva@abv.bg>
2024-11-24 15:32:14 +0200

    Add ACF to project, box to testemonials

--- wp-content/themes/softuni/single-testimonials.php
+++ wp-content/themes/softuni/single-testimonials.php
@@ -1,6 +1,16 @@
 <?php get_header(); ?>
 
-	<section id="about" class="scrollspy-section padding-xlarge">
+
+<?php
+$id = get_the_ID();
+$testimonial_author = get_field( 'author', $id );
+$testimonial_image = get_field( 'author_image', $id );
+//var_dump ( $testimonial_author);
+//var_dump ( $testimonial_image);
+?>
+
+
+<section id="about" class="scrollspy-section padding-xlarge">
 		<div class="container">
 			<div class="row">
     			<div class="col-md-12">
@@ -23,11 +33,21 @@
 	    			<div class="col-md-6 description text-lead">
 				    	<?php the_content(); ?>
 			    	</div>
-			</div>
+
+					<div class="col-md-6 description text-lead">
+    		            <?php if ( ! empty( $testimonial_author ) ) : ?>
+	    	                <div><?php echo esc_attr( $testimonial_author ) ?></div>
+		                <?php endif; ?>
+    		            <?php if ( ! empty( $testimonial_image )) : ?>
+        		            <div class="testimonial-author">
+            		            <img src="<?php echo $testimonial_image ?>">
+            		        </div>
+            		    <?php endif; ?>
+        		    </div>
+				</div>
 
 		</div>
 	</section>
 
-	<?php softuni_display_latest_posts( 2 ); ?>
 
 <?php get_footer(); ?>
